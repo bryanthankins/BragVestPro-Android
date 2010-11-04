b0f525908c280027e813190a6242e8ce99c271ac
Bryant Hankins <bryanthankins@gmail.com>
2010-11-03 23:05:03 -0500

    changed to use activity and viewflipper

--- gen/com/bragvest/bragvestpro/R.java
+++ gen/com/bragvest/bragvestpro/R.java
@@ -14,6 +14,10 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int ListView01=0x7f050002;
+        public static final int ListView02=0x7f050003;
+        public static final int ListView03=0x7f050004;
+        public static final int ViewFlipper01=0x7f050001;
         public static final int name_entry=0x7f050000;
     }
     public static final class layout {
--- src/com/bragvest/bragvestpro/BragVestPro.java
+++ src/com/bragvest/bragvestpro/BragVestPro.java
@@ -13,29 +13,31 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.SimpleCursorAdapter;
 
-public class BragVestPro extends ListActivity {
+public class BragVestPro extends Activity {
     /** Called when the activity is first created. */
     private static String[] FROM = { "name", "_id" };
     private static int[] TO = { R.id.name_entry};
     private static String ORDER_BY = "_id DESC";
     private static String TABLE_NAME = "ranks";
     private DataBaseHelper dbHelper;
+    private ViewFlipper flipper;
 
     @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
+          
+            //Set initial view
+            setContentView(R.layout.main);
+            
             // Get data from database
             SQLiteDatabase db;
             dbHelper = new DataBaseHelper(this);
-          //  try {
-                dbHelper.createDataBase();
-          //  } catch (IOException ioe) {
-          //      throw new Error("Unable to create database");
-         //   }
+            dbHelper.createDataBase();
             try {
                 db = dbHelper.openDataBase();
             }catch(SQLException sqle){
@@ -46,21 +48,29 @@ public class BragVestPro extends ListActivity {
                     null,null);
             startManagingCursor(cursor);
             
+            
             // Tie to UI
             SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
                     R.layout.list_item, cursor, FROM, TO);
-            setListAdapter(adapter);
-            ListView lv = getListView();
-            lv.setTextFilterEnabled(true);
+            ListView ranks = (ListView)findViewById(R.id.ListView01);
+            ranks.setAdapter(adapter);
+
+
+            
+            
 
             // Handle click event
-            lv.setOnItemClickListener(new ListView.OnItemClickListener() {
+            ranks.setOnItemClickListener(new ListView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View view,
                     int position, long id) {
-                    // When clicked, show a toast with the TextView text
-                    Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
-                        Toast.LENGTH_SHORT).show();
+                	//TODO: load up second list view with badge data based on selection
+                    flipper.showNext();
                 }
             });
+
         }
+   /* @Override
+        public void onClick(View v) {
+                flipper.showNext();
+        }*/
 }
